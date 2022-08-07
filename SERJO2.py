d38a44a22689fc73e706ccce2453a6fa73639ffc
سيرجو ال صبراتي <102861873+serjo20@users.noreply.github.com>
2022-08-07 22:50:59 +0200

    Add files via upload

---
+++ SERJO2.py
@@ -0,0 +1,16 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Qadir import main
+    main()
+elif bit == '32bit':
+    from Qadir32 import main
+    main()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
